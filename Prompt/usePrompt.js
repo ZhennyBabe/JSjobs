04c05fbbf880405c99b36d705226390643d0084d
zheny <526418933@qq.com>
2020-11-05 20:39:28 +0800

    看看的额

--- Prompt/usePrompt.js
+++ Prompt/usePrompt.js
@@ -1,6 +1,13 @@
 window.onload=function () {
-    let promptResult = this.prompt("Please input your name:");
-    if(promptResult){
-        this.alert("Hi,"+promptResult+"!");
+    // let promptResult = this.prompt("Please input your name:");
+    // if(promptResult){
+    //     this.alert("Hi,"+promptResult+"!");
+    // }   
+    let i =3;
+    if(i>5){
+        this.alert(i);
+    }else{
+        //continue;
     }
+    alert(i);
 }
