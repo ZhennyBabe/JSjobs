9b7d5038548a699a2bfced23399840cdcf5f673b
zheny <526418933@qq.com>
2020-11-05 20:48:03 +0800

    Prompt

--- Prompt/usePrompt.js
+++ Prompt/usePrompt.js
@@ -1,13 +1,13 @@
 window.onload=function () {
-    // let promptResult = this.prompt("Please input your name:");
-    // if(promptResult){
-    //     this.alert("Hi,"+promptResult+"!");
-    // }   
+ let promptResult = this.prompt("Please input your name:");
+ if(promptResult){
+    this.alert("Hi,"+promptResult+"!");
+ }   
     let i =3;
     if(i>5){
         this.alert(i);
     }else{
         //continue;
     }
-    alert(i);
+    alert(i+1);
 }
